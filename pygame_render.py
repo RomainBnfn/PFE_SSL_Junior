cfdae11b635a9c8791d783aa902308aa003a6f95
RomainBnfn <61451498+RomainBnfn@users.noreply.github.com>
2022-01-10 00:24:03 +0100

    robot display

--- pygame_render.py
+++ pygame_render.py
@@ -4,51 +4,76 @@ import math
 
 # Constants
 
-#
-FIELD_WIDTH = 1800  #mm
-FIELD_HEIGHT = 1200 #mm
-A = 300             #mm
-B = 460             #mm (from left corner)
-RATIO = FIELD_HEIGHT / FIELD_WIDTH 
+# Mesured done on PDF
+FIELD_WIDTH, FIELD_HEIGHT = 1200 , 1800 #mm
+X_POS, Y_POS = 300, 440                 #mm 
+BUT = 160                               #mm to 1200-160
+RATIO = FIELD_HEIGHT / FIELD_WIDTH
+# 
 FIELD_COLOR = (68, 170, 0)
 #
-BORDER = 50
-SCREEN_WIDTH = 1000 + BORDER
+# ROBOT
+ROBOT_MARK = 100 #mm
+ROBOT_SIZE = 180 #mm
+
+
+
+BORDER = 100
+SCREEN_WIDTH = 450 + BORDER
 SCREEN_HEIGHT = (SCREEN_WIDTH-BORDER) * RATIO + BORDER
 
-class IRenderable:
-    def display(self):
-        pass
+BALL_SIZE = 14 # to define
+ROBOT_SIZE = 30 # to define
+        
+RATIO_MM_PIXEL = FIELD_WIDTH/(SCREEN_WIDTH-BORDER)
+def fromMMtoPixel(x, y):  
+    return x / RATIO_MM_PIXEL + SCREEN_WIDTH/2, y / RATIO_MM_PIXEL + SCREEN_HEIGHT/2
 
-class Robot(IRenderable):
-    def __init__(self):
-        pass
-    def display(self):
-        pass
-
-class Ball(IRenderable):
-    def __init__(self):
-        pass
-    def display(self):
-        pass
-    
+def fromMMtoPixel_dist(d):
+    return d * RATIO_MM_PIXEL 
 class PygameRender:
     def __init__(self):
         pygame.init()
         pygame.font.init()
         self._screen = None
-        self.background = pygame.image.load("assets\\images\\croped_field.png")
-        self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH-BORDER, SCREEN_HEIGHT-BORDER))
-    
+        # Load field image
+        self.field_image = pygame.image.load("assets\\images\\croped_field.png")
+        self.field_image = pygame.transform.rotate(self.field_image, 90)
+        self.field_image = pygame.transform.scale(self.field_image, (SCREEN_WIDTH-BORDER, SCREEN_HEIGHT-BORDER))
+        # Load robot images 
+        self.blue_marker_1 = pygame.image.load("assets\\images\\blue-markers1.png")
+        
     def render(self, field):
         width, height = 1500, 800 #field.width, field.height # in cm  
-        self.screen.fill((40, 40, 40))    
-        self.screen.blit(self.background, (BORDER/2, BORDER/2))
+        self.screen.fill( FIELD_COLOR )    
+        self.screen.blit(self.field_image, (BORDER/2, BORDER/2))
         #pygame.draw.rect(self.screen, (20, 180, 20), (0.05*SCREEN_WIDTH, 0.05*SCREEN_HEIGHT, 0.90*SCREEN_WIDTH, 0.90*SCREEN_HEIGHT))
 
+        self.display_ball(0,0)
+        self.display_ball(X_POS, -Y_POS)
+        self.display_robot(244, -20, 34, 'blue', 1)
+        
         pygame.display.update()
         pygame.time.delay(3000)
 
+    def display_ball(self, x, y):
+        x_render, y_render = fromMMtoPixel(x,y)
+        pygame.draw.circle(self.screen, (255, 255, 0), (x_render, y_render), BALL_SIZE/2)
+    
+    def display_robot(self, x, y, o, color, number):
+        x_render, y_render = fromMMtoPixel(x,y)
+        if color == 'red':
+            c = (255, 0, 0)
+        else:
+            c = (0, 0, 255)
+            if number == 1:
+                image = self.blue_marker_1
+        image = pygame.transform.scale(image, (50, 50))
+        image = pygame.transform.rotate(image, o)
+        pygame.draw.circle(self.screen, c, (x_render, y_render), fromMMtoPixel_dist(ROBOT_SIZE)/2)
+        self.screen.blit(image, (x_render-25, y_render-25))
+        
+        
     @property
     def screen(self):
         if self._screen is None:
@@ -57,3 +82,4 @@ class PygameRender:
     
 test = PygameRender()
 test.render("")
+
